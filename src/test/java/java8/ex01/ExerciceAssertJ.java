ac7d38c44d18fe63e361fd83926593841b241987
StevenD <dellarica.steven@gmail.com>
2024-10-17 11:52:33 +0200

    TP Excercice 2.1 et 2.2

---
+++ src/test/java/java8/ex01/ExerciceAssertJ.java
@@ -0,0 +1,66 @@
+package java8.ex01;
+
+import java8.data.Data;
+import java8.data.domain.Pizza;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.*;
+
+
+/**
+ * Assertions Hamcrest
+ */
+public class ExerciceAssertJ {
+
+    @Test
+    public void test_is() throws Exception {
+        List<Pizza> pizzas = new Data().getPizzas();
+
+        Boolean result =  pizzas.stream().anyMatch(p -> p.getPrice() >= 1300);
+
+        // Valider que result vaut true
+        assertThat(result).isTrue();
+
+    }
+
+    @Test
+    public void test_hasSize() throws Exception {
+        List<Pizza> pizzas = new Data().getPizzas();
+
+        List<Pizza> result =  pizzas.stream()
+                .filter(p -> p.getPrice() >= 1300)
+                .collect(toList());
+
+        // Valider que result a 3 elements
+        assertThat(result).hasSize(3);
+    }
+
+
+    @Test
+    public void test_hasProperty() throws Exception {
+        List<Pizza> pizzas = new Data().getPizzas();
+
+        Pizza result = pizzas.stream().max(Comparator.comparing(Pizza::getPrice)).orElseThrow();
+
+        // Valider que result a la propriété name qui vaut "La Cannibale"
+        assertThat(result).hasFieldOrPropertyWithValue("name", "La Cannibale");
+    }
+
+    @Test
+    public void test_everyItem() throws Exception {
+        List<Pizza> pizzas = new Data().getPizzas();
+
+        List<Pizza> result =  pizzas.stream()
+                .filter(p -> p.getPrice() >= 1300)
+                .collect(toList());
+
+        // Valider que tous les objets Pizza de la liste result ont un prix >= 1300
+        assertThat(result).allMatch(p -> p.getPrice() >= 1300);
+
+    }
+
+}
